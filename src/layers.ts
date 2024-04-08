ae6ba5e9d5094edc8db2172b2363cb28eaa03610
EijiGorilla <emasuz@hotmail.com>
2024-04-08 09:28:58 +0800

    updated

--- src/layers.ts
+++ src/layers.ts
@@ -235,7 +235,7 @@ export const popuTemplate = {
 };
 
 const colorStatus = [
-  [225, 225, 225], // To be Constructed (white)
+  [225, 225, 225, 0.1], // To be Constructed (white)
   [130, 130, 130, 0.5], // Under Construction
   [255, 0, 0, 0.8], // Delayed
   [0, 112, 255, 0.8], // Completed
